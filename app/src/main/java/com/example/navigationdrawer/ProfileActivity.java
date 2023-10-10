8be5b4f302f1b70b02db239308528d5edbe455c8
khangfutsal <khangfutsal2@gmail.com>
2023-10-10 14:33:12 +0700

    10102023 1433am

--- app/src/main/java/com/example/navigationdrawer/ProfileActivity.java
+++ app/src/main/java/com/example/navigationdrawer/ProfileActivity.java
@@ -2,19 +2,24 @@ package com.example.navigationdrawer;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import com.example.navigationdrawer.databinding.ActivityProfileBinding;
+import com.github.dhaval2404.imagepicker.ImagePicker;
 
 public class ProfileActivity extends AppCompatActivity {
-    ImageView imgView_Back;
+    ImageView imgView_Back,img_User;
     Button btn_Change_Password;
     ProfileActivity_ModelView profileActivityModelView = new ProfileActivity_ModelView();
     @SuppressLint("MissingInflatedId")
@@ -31,6 +36,8 @@ public class ProfileActivity extends AppCompatActivity {
 
     void AnhXa(){
         imgView_Back = (ImageView)findViewById(R.id.back_profile);
+        img_User = (ImageView) findViewById(R.id.imgUser_ProfilePage);
+
         btn_Change_Password = (Button)findViewById(R.id.changepass);
     }
 
@@ -38,8 +45,11 @@ public class ProfileActivity extends AppCompatActivity {
         imgView_Back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+//                startActivity(intent);
+                ImagePicker.with(ProfileActivity.this)
+                        .galleryOnly()
+                        .start();
             }
         });
 
@@ -50,5 +60,14 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri = data.getData();
+        Toast.makeText(this, "uri "+uri, Toast.LENGTH_SHORT).show();
+        img_User.setImageURI(uri);
+
     }
 }
