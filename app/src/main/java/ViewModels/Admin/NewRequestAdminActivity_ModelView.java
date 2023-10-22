bd778c94685e3fb2ba421ee9da3adad37a0ab821
khangfutsal <khangfutsal2@gmail.com>
2023-10-22 19:39:57 +0700

    Update 1939pm 22102023

--- app/src/main/java/ViewModels/Admin/NewRequestAdminActivity_ModelView.java
+++ app/src/main/java/ViewModels/Admin/NewRequestAdminActivity_ModelView.java
@@ -4,12 +4,17 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
 
 import com.example.navigationdrawer.BR;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import Models.Asset;
@@ -43,22 +48,55 @@ public class NewRequestAdminActivity_ModelView extends BaseObservable {
         return u.GetUserAdapter();
     }
 
-    public void OnClickSaveButton(Context context){
+    public void OnClickSaveButton(Context context) {
         if(date == null){
             return;
         }
-        SharedPreferences sharedPreferences = context.getSharedPreferences("Assignment", Context.MODE_PRIVATE);
-        String categoryID = sharedPreferences.getString("Category_id","");
-        String userID = sharedPreferences.getString("User_id","");
-        String assetID = sharedPreferences.getString("Asset_id","");
+        Calendar calendar = Calendar.getInstance();
 
-        User u = new User();
-        String adminID = u.GetNameUserByEmail(MyApplication.getInstance().GetSharedData());
+        int dateCalendar = calendar.get(Calendar.DATE);
+        int monthCalendar = calendar.get(Calendar.MONTH);
+        int yearCalendar = calendar.get(Calendar.YEAR);
+        calendar.set(yearCalendar,monthCalendar,dateCalendar);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String currentDate_Str = simpleDateFormat.format(calendar.getTime());
 
-        Assignment a = new Assignment(assetID,categoryID,userID,adminID,date,"2023-02-03","1","1");
-        a.AddAssignment();
 
-        ((Activity) context).finish();
+        Date currentDate = null;
+        try {
+            currentDate = simpleDateFormat.parse(currentDate_Str);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        Date dateOfUser = null;
+        try {
+            dateOfUser = simpleDateFormat.parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        if(dateOfUser.compareTo(currentDate) > 0){
+            SharedPreferences sharedPreferences = context.getSharedPreferences("Assignment", Context.MODE_PRIVATE);
+            String categoryID = sharedPreferences.getString("Category_id","");
+            String userID = sharedPreferences.getString("User_id","");
+            String assetID = sharedPreferences.getString("Asset_id","");
+
+            User u = new User();
+            String adminID = u.GetNameUserByEmail(MyApplication.getInstance().GetSharedData());
+
+            Assignment a = new Assignment(assetID,categoryID,userID,adminID,currentDate_Str,date,"","1");
+            a.AddAssignment();
+
+            ((Activity) context).finish();
+        }
+        else if(dateOfUser.compareTo(currentDate) < 0){
+            Toast.makeText(context, "Ngay Thang Nam Phai lon hon ngay hien tai ", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else{
+            return;
+        }
 
     }
 }
